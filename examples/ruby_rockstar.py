c6f3ad26e3fcebf47069ceb35f12fed7d733376c
Dan Bruns <danielbruns-wf@users.noreply.github.com>
2015-07-21 11:22:42 -0500

    Update ruby_rockstar.py

--- examples/ruby_rockstar.py
+++ examples/ruby_rockstar.py
@@ -1,5 +1,5 @@
-from rockstar import RockStar
+from RockStar import RockStar
 
 ruby_code = "puts 'Hello world'"
-rock_it_bro = RockStar(days=400, file_name='helloWorld.rb', code=ruby_code)
+rock_it_bro = RockStar(days=1080, file_name='helloWorld.rb', code=ruby_code)
 rock_it_bro.make_me_a_rockstar()
